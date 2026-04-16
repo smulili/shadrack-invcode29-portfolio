6c28871a74a60c6186e248ed074821ba34fb9c0c
shaddev-254 <shadrackmwendwa38@gmail.com>
2026-04-16 15:28:56 +0300

    links, build

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,6 +7,7 @@ const NotFound = () => {
   useEffect(() => {
     console.error("404 Error: User attempted to access non-existent route:", location.pathname);
   }, [location.pathname]);
+  
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted">
